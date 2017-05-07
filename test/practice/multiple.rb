68966809ac6b05a1b83cb71ad55ea6b1f4e9ba28
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-05-07 16:26:36 -0400

    test unit gem added so model test commands can run

--- test/practice/multiple.rb
+++
@@ -1,5 +0,0 @@
-module Multiple
-  def self.multiple_of?(multiple, num)
-    multiple % num == 0
-  end
-end
--- test/practice/multiple_test.rb
+++
@@ -1,8 +0,0 @@
-require "test/unit"
-require_relative "multiple"
-
-class MultipleTest < Test::Unit::TestCase
-  def test_multiple_of
-    assert Multiple.multiple_of?(10,5), "10 should be a multiple of 5"
-  end
-end
