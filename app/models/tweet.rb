a8f9932edc519ed54d5a0d03c49d2521ad312c79
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-05-07 20:51:04 -0400

    tweet test practices

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,23 @@
-class Tweet < ApplicationRecord
-  validates :status, presence: true
+class Tweet < ActiveRecord::Base
+  belongs_to :zombie
 
-  belongs_to :zombie 
+  validates :status, presence: true, length: {within: 3..140, allow_blank: true}
+  validates :zombie, presence: true
+
+  def brains?
+    status =~ /(brains|brains)/i
+  end
+
+  def show_author_summary
+    self.status = self.zombie.zombie_summary
+  end
+
+  def status_image
+    image = ZwitPic.get_status_image(self.id)
+
+    #returning an array
+    {image_name: image[0], image_url: image[1]} 
+  end
+
+
 end
--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+
+  test "invalid without a status" do
+    tweet = Tweet.new
+    assert !tweet.valid?, "Tweet not valid without a status"
+  end
+
+  # # validating tweet
+  # test "valid with all attributes" do
+  #   zombie = Zombie.new
+  #   tweet = Tweet.new
+  #
+  #   tweet.status = "I want to eat your brains!"
+  #   tweet.zombie = zombie
+  #
+  #   assert tweet.valid?, "Tweet was not valid"
   # end
+
+  # validating tweet using fixtures
+  test "valid with all attributes" do
+    tweet = tweets(:ash_1)
+
+    assert tweet.valid?, "tweet isn't valid"
+  end
+
+  test "can detect brains" do
+    tweet = tweets(:ash_2)
+
+    assert tweet.brains?, "tweet contains brains"
+  end
+
+  test "contains a zombie" do
+    tweet = tweets(:ash_2)
+    assert_equal zombies(:ash), tweet.zombie, "tweet doesn't contain a zombie"
+
+ end
+
+
 end
